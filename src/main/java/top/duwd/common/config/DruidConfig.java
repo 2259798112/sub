a17fcfe2208812bd9b52425dad6a423e1e9bef34
du-wd <du.wd@outlook.com>
2020-05-14 16:26:04 +0800

    e-job

--- src/main/java/top/duwd/common/config/DruidConfig.java
+++ src/main/java/top/duwd/common/config/DruidConfig.java
@@ -23,43 +23,43 @@ public class DruidConfig {
     @Value("${spring.datasource.password}")
     private String password;
 
-    @Value("${spring.datasource.driver-class-name}")
+    @Value("${spring.datasource.driverClassName}")
     private String driverClassName;
 
-    @Value("${spring.datasource.initial-size}")
+    @Value("${spring.datasource.initialSize}")
     private int initialSize;
 
-    @Value("${spring.datasource.min-idle}")
+    @Value("${spring.datasource.minIdle}")
     private int minIdle;
 
-    @Value("${spring.datasource.max-active}")
+    @Value("${spring.datasource.maxActive}")
     private int maxActive;
 
-    @Value("${spring.datasource.max-wait}")
+    @Value("${spring.datasource.maxWait}")
     private int maxWait;
 
-    @Value("${spring.datasource.time-between-eviction-runs-millis}")
+    @Value("${spring.datasource.timeBetweenEvictionRunsMillis}")
     private int timeBetweenEvictionRunsMillis;
 
-    @Value("${spring.datasource.min-evictable-idle-time-millis}")
+    @Value("${spring.datasource.minEvictableIdleTimeMillis}")
     private int minEvictableIdleTimeMillis;
 
 //    @Value("${spring.datasource.validation-query}")
 //    private String validationQuery;
 
-    @Value("${spring.datasource.test-while-idle}")
+    @Value("${spring.datasource.testWhileIdle}")
     private boolean testWhileIdle;
 
-    @Value("${spring.datasource.test-on-borrow}")
+    @Value("${spring.datasource.testOnBorrow}")
     private boolean testOnBorrow;
 
-    @Value("${spring.datasource.test-on-return}")
+    @Value("${spring.datasource.testOnReturn}")
     private boolean testOnReturn;
 
-    @Value("${spring.datasource.pool-prepared-statements}")
+    @Value("${spring.datasource.poolPreparedStatements}")
     private boolean poolPreparedStatements;
 
-    @Value("${spring.datasource.max-pool-prepared-statement-per-connection-size}")
+    @Value("${spring.datasource.maxPoolPreparedStatementPerConnectionSize}")
     private int maxPoolPreparedStatementPerConnectionSize;
 
     @Value("${spring.datasource.filters}")
