c79b76547c6b98527313b254d0fc3c51f8420020
225 <225>
2020-06-11 21:45:46 +0800

    zhihu hot

--- src/main/java/top/duwd/common/mapper/sub/ZhihuHotMapper.java
+++ src/main/java/top/duwd/common/mapper/sub/ZhihuHotMapper.java
@@ -12,7 +12,7 @@ public interface ZhihuHotMapper extends Mapper<ZhihuHot> {
     @Select("SELECT count(*) from t_zhihu_hot t where t.qid=#{param1} ")
     int selectCountByQid(Integer qid);
 
-    @Select("SELECT count(*) from t_zhihu_hot t where t.create_time = t.update_time limit #{param1}")
+    @Select("SELECT t.* from t_zhihu_hot t where t.create_time = t.update_time limit #{param1}")
     @ResultMap("BaseResultMap")
     List<ZhihuHot> findList(int limit);
 }
